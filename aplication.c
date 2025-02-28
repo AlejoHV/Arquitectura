350b5a470f0538624473c351a2c63782004e3045
Ubuntu <ubuntu@ip-172-31-87-120.ec2.internal>
2025-02-28 16:26:43 +0000

    conexion a github

---
+++ aplication.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Arreglo de strings char **
+
+//Guarda con Control O (enter)
+//Sale con Control X
+
+//La funcion main es el entry point (punto de entrada) de un ejecutable de lenguaje c 
+int main(void){
+  printf("\nHola mundo!\n");
+  return 0;
+}
+
+
